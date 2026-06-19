dcb00ddc3fdc3963a2ded4e495b5d2c03423b44b
pankajsingh rajpurohit <pankajsinh11223@gmail.com>
2026-06-19 16:56:44 +0530

    add update route

--- Backend/Controllers/ProjectController.js
+++ Backend/Controllers/ProjectController.js
@@ -3,43 +3,39 @@ import db from "../Config/db.js";
 
 
 export const getProjects = async (req, res) => {
-    const query = "SELECT * FROM projects";
-
-    db.query(query, (err, result) => {
-
-        if (err) {
-            return res.status(500).json({
-                success: false,
-                message: err.message,
-            })
-        }
+    try {
+        const [projects] = await db.promise().query("SELECT * FROM projects");
 
         res.status(200).json({
             success: true,
-            count: result.length,
-            data: result
+            count: projects.length,
+            data: projects,
         })
-    })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+
+    }
 };
 
-export const getProjectsBySlug = (req, res) => {
 
-    const { slug } = req.params;
 
-    const projectQuery = " SELECT * FROM projects WHERE slug = ? ";
+export const getProjectsBySlug = async (req, res) => {
+    try {
 
-    db.query(projectQuery, [slug], (err, projectResult) => {
-        if (err) {
-            return res.status(500).json({
-                success: false,
-                message: err.message
-            })
-        }
 
-        if (projectResult.length === 0) {
+        const { slug } = req.params;
+
+        const projectQuery = " SELECT * FROM projects WHERE slug = ? ";
+
+        const [projectResult] = await db.promise().query(projectQuery, [slug]);
+
+        if (!projectResult.length) {
             return res.status(404).json({
                 success: false,
-                message: "Project Not Found",
+                message: "Project Not Found"
             })
         }
 
@@ -47,32 +43,224 @@ export const getProjectsBySlug = (req, res) => {
 
         const serviceQuery = "SELECT service_name FROM services WHERE project_id = ? ";
 
-        db.query(serviceQuery, [project.id], (err, serviceResult) => {
-            if (err) {
-                return res.status(500).json({
-                    success: false,
-                    message: err.message
-                })
-            }
+        const [serviceResult] = await db.promise().query(serviceQuery, [project.id]);
 
-            const galleryQuery = "SELECT image_url FROM gallery WHERE project_id = ? ";
+        const galleryQuery = "SELECT image_url FROM gallery WHERE project_id = ? ";
 
-            db.query(galleryQuery, [project.id], (err, galleryResult) => {
-                if (err) {
-                    return res.status(500).json({
-                        success: false,
-                        message: err.message
-                    });
-                }
+        const [galleryResult] = await db.promise().query(galleryQuery, [project.id]);
 
-                res.status(200).json({
-                    success: true,
-                    project,
-                    services: serviceResult,
-                    gallery: galleryResult
 
-                })
-            })
+        res.status(200).json({
+            success: true,
+            project,
+            services: serviceResult,
+            gallery: galleryResult
+
         })
-    })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
+export const createProject = async (req, res) => {
+
+    try {
+
+
+        const {
+            slug,
+            title,
+            category,
+            client,
+            year,
+            duration,
+            description,
+            challenge,
+            solution,
+            heroImage,
+            traffic,
+            conversions,
+            engagement, services, gallery
+        } = req.body;
+
+        const query = ` INSERT INTO projects(slug,
+        title,
+        category,
+        client,
+        year,
+        duration,
+        description,
+        challenge,
+        solution,
+        heroImage,
+        traffic,
+        conversions,
+        engagement) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+
+        const [result] = await db.promise().query(query, [slug,
+            title,
+            category,
+            client,
+            year,
+            duration,
+            description,
+            challenge,
+            solution,
+            heroImage,
+            traffic,
+            conversions,
+            engagement],)
+
+        const projectId = result.insertId;
+
+        if (services?.length > 0) {
+            const serviceValues = services.map((service) => [projectId, service])
+
+            await db.promise().query(`INSERT INTO services (project_id ,service_name) VALUES ?`, [serviceValues])
+        }
+
+        if (gallery?.length > 0) {
+            const galleryValues = gallery.map((image) => [projectId, image])
+
+            await db.promise().query(`INSERT INTO gallery (project_id ,image_url) VALUES ?`, [galleryValues])
+        }
+
+        res.status(201).json({
+            success: true,
+            message: "Product Created",
+            projectId,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const deleteProject = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await db.promise().query(`DELETE FROM projects WHERE id=? `, [id]);
+
+        res.status(200).json({
+            success: true,
+            message: "project deleted successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+
+    }
 }
+
+export const updateProject = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const { services, gallery, ...project } = req.body;
+
+        const query = `UPDATE projects SET 
+          slug= ? ,
+          title= ?,
+          category=?,
+          client=? ,
+          year=? ,
+          duration=? ,
+          description=?,
+          challenge=?,
+          solution=?,
+          heroImage=?,
+          traffic=?,
+          conversions=?,
+          engagement=?
+          WHERE id =? `
+
+        const values = [
+            project.slug,
+            project.title,
+            project.category,
+            project.client,
+            project.year,
+            project.duration,
+            project.description,
+            project.challenge,
+            project.solution,
+            project.heroImage,
+            project.traffic,
+            project.conversions,
+            project.engagement,
+            id,
+
+        ]
+
+        await db.promise().query(query, values);
+
+        // Update Services
+
+        await db.promise().query(
+            "DELETE FROM services WHERE project_id = ?",
+            [id]
+        );
+
+        if (services?.length > 0) {
+            const serviceValues = services.map(
+                (service) => [
+                    id,
+                    service,
+                ]
+            );
+
+            await db.promise().query(
+                `
+        INSERT INTO services
+        (project_id, service_name)
+        VALUES ?
+        `,
+                [serviceValues]
+            );
+        }
+
+        // Update Gallery
+
+        await db.promise().query(
+            "DELETE FROM gallery WHERE project_id = ?",
+            [id]
+        );
+
+        if (gallery?.length > 0) {
+            const galleryValues = gallery.map(
+                (image) => [
+                    id,
+                    image,
+                ]
+            );
+
+            await db.promise().query(
+                `
+        INSERT INTO gallery
+        (project_id, image_url)
+        VALUES ?
+        `, [galleryValues]
+            );
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Project Updated Successfully",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
--- Backend/Routes/ProjectRoute.js
+++ Backend/Routes/ProjectRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { getProjects, getProjectsBySlug } from "../Controllers/ProjectController.js";
+import { createProject, deleteProject, getProjects, getProjectsBySlug, updateProject } from "../Controllers/ProjectController.js";
 
 const ProjectRouter = express.Router();
 
 ProjectRouter.get("/", getProjects);
+ProjectRouter.post("/", createProject);
 ProjectRouter.get("/:slug", getProjectsBySlug)
+ProjectRouter.delete("/:id", deleteProject)
+ProjectRouter.put("/:id", updateProject)
 
 export default ProjectRouter;
